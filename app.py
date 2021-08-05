a03fecec2d05c85bc0eaedcafdf0fa277e11808a
Simon Denisov <simondnsv@gmail.com>
2021-08-04 17:33:26 -0700

    Formula 2 standings web scraping app

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import requests
+from bs4 import BeautifulSoup as BS
 
+response = requests.get(
+    "https://www.fiaformula2.com/Standings/Driver")
+soup = BS(response.content, 'html.parser')
+
+standings = soup.select(".driver-name-wrapper")
+
+
+for drivers in standings:
+    driver = drivers.select(".driver-name > .visible-desktop-up")
+    num = drivers.select(".pos")
+    print(driver[0].text)
+    print(num[0].text)
